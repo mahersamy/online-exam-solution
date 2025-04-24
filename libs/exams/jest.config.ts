96f7a55f8845a360948638567b657b23e9c05a42
maher <mahersamy699@yahoo.com>
2025-04-24 13:29:53 +0200

    create exams lib

---
+++ libs/exams/jest.config.ts
@@ -0,0 +1,21 @@
+export default {
+  displayName: 'exams',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageDirectory: '../../coverage/libs/exams',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
--- libs/subjects/src/index.ts
+++ libs/subjects/src/index.ts
@@ -2,5 +2,3 @@ export * from './lib/subjects/subjects.component';
 export * from './lib/services/subject-api.service';
 export * from './lib/interfaces/subject-response';
 export * from './lib/token/api-token';
-
-// SubjectResponse
